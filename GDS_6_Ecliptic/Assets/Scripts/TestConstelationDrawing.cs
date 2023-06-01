e634fd0d2b51dea6d3bfea0550d88b5066cef708
WillDoesDevelopment <s3901070@student.rmit.edu.au>
2023-06-01 17:31:14 +1000

    working on Constellation drawing

--- GDS_6_Ecliptic/Assets/Scripts/TestConstelationDrawing.cs
+++ GDS_6_Ecliptic/Assets/Scripts/TestConstelationDrawing.cs
@@ -5,6 +5,8 @@ using UnityEngine.EventSystems;
 public class TestConstelationDrawing : MonoBehaviour
 {
     public LayerMask layer;
+
+    public GameObject FollowObj;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,33 +14,30 @@ public class TestConstelationDrawing : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        // this is option 1, This only tells us if the mouse is over a ui element (No 2d box collider needed) however does not retrieve Object info
-/*        if (UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
-        {
-            //Debug.Log("Working");
-
-        }*/
         raycasting();
-
+        mouseFollow();
     }
     public void raycasting()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //mousePosition.z = Mathf.Infinity;
-        RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero, Mathf.Infinity, layer);
+        PointerEventData pointer = new PointerEventData(EventSystem.current);
+        pointer.position = Input.mousePosition;
 
-        if (hit.collider != null)
+        List<RaycastResult> raycastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointer, raycastResults);
+
+        if (raycastResults.Count > 0)
         {
-            Debug.Log(hit.transform.gameObject.name);
+            foreach (var go in raycastResults)
+            {
+                Debug.Log(go.gameObject.name, go.gameObject);
+            }
         }
-
-
     }
-    public void OnMouseEnter()
+
+    public void mouseFollow()
     {
-        // this only works when the script it on the UI elements. Less ideal
+        FollowObj.transform.position = Input.mousePosition;
     }
-
 }
