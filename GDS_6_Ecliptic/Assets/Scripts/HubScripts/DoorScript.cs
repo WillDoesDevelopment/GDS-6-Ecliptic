a1d3ed6e7f20babb2310d760faa11d7b25a27889
WillDoesDevelopment <s3901070@student.rmit.edu.au>
2023-07-20 16:39:30 +1000

    MainHub framework set up
    All scripts have been made and code has been planned out

---
+++ GDS_6_Ecliptic/Assets/Scripts/HubScripts/DoorScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public DoorStatus DS;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GDS_6_Ecliptic/Assets/Scripts/HubScripts/DoorStatus.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class DoorStatus 
+{
+    public int SceneNum;
+    public bool IsOpen;
+}
