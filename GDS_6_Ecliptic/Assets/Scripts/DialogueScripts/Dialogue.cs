08b381d6b97ce59e2fe6570e921116b804c0f26a
WillDoesDevelopment <s3901070@student.rmit.edu.au>
2023-08-02 19:43:06 +1000

    Reworking dialogue system

--- GDS_6_Ecliptic/Assets/Scripts/DialogueScripts/Dialogue.cs
+++ GDS_6_Ecliptic/Assets/Scripts/DialogueScripts/Dialogue.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // so you can see this in editor
-[System.Serializable]
-public class Dialogue 
+//[System.Serializable]
+[CreateAssetMenu(menuName = "data/dialogue")]
+public class Dialogue: ScriptableObject
 {
-    
+    [System.Serializable]
+    public struct DialogueLine 
+    { 
+        public string sentence; 
+        public DialogueType dialogueType;
+    }
+
+
+    public DialogueLine[] line;
     // all required information for the dialogue
     public enum DialogueType
     {
