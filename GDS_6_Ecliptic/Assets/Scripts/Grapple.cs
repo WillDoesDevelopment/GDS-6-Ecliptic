05ab821e0750aca8e09a8c3a2ce621c5da440417
GylesGyles <gyles.whytcross@gmail.com>
2023-08-09 13:05:50 +1000

    Grapple VFX

--- GDS_6_Ecliptic/Assets/Scripts/Grapple.cs
+++ GDS_6_Ecliptic/Assets/Scripts/Grapple.cs
@@ -11,18 +11,21 @@ public class Grapple : MonoBehaviour
     public float length = 10f;
     public float t = 1;
     public float ropeWidth = 1;
-    public float maxLength = 10f;
+    public float maxLength = 10f;    
     public LayerMask grappleLayer;
 
     Transform playerPoint;
     Transform grapplePoint;
     Vector3 playerXZ;
+    Renderer rend;
 
     // Start is called before the first frame update
     void Start()
     {
         rope.SetActive(false);
         scope.SetActive(false);
+
+        rend = rope.GetComponent<Renderer>();                
     }
 
     // Update is called once per frame
@@ -71,15 +74,17 @@ public class Grapple : MonoBehaviour
         }
 
 
-
-
-
         //Object follow
 
         if (target != null)
         {
+            playerPoint = transform;
+            grapplePoint = target.transform;
+            length = Vector3.Distance(playerPoint.position, grapplePoint.position);
+
+            //Target
             playerXZ = new Vector3(transform.position.x, target.transform.position.y, transform.position.z);
-            t = length / Vector3.Distance(target.transform.position, transform.position);
+            t = maxLength / length;
             if (t < 1)
             {
                 //target.transform.LookAt(playerXZ);            
@@ -87,13 +92,15 @@ public class Grapple : MonoBehaviour
             target.transform.position = Vector3.Lerp(playerXZ, target.transform.position, t);
 
             //Rope
-            playerPoint = transform;
-            grapplePoint = target.transform;
-
             rope.transform.position = (playerPoint.position + grapplePoint.position) / 2.0f;
-            rope.transform.localScale = new Vector3(ropeWidth, ropeWidth, Vector3.Distance(playerPoint.position, grapplePoint.position));
+            rope.transform.localScale = new Vector3(ropeWidth, ropeWidth, length);
             rope.transform.LookAt(grapplePoint);
 
+
+            rend.material.SetFloat("_Stretch_Amount", length / maxLength);
+
+
+            //column interaction
             if (target.GetComponent<columnScript>() != null)
             {
                 //Debug.Log("almost");
