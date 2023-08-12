65f02aa80c0f0873acbd992dc0aade675a7e6818
GylesGyles <gyles.whytcross@gmail.com>
2023-08-12 18:49:26 +1000

    Aries animation and obstacle adjustment
    Looks playable now

--- GDS_6_Ecliptic/Assets/Scripts/AriesScripts/BasicCamera.cs
+++ GDS_6_Ecliptic/Assets/Scripts/AriesScripts/BasicCamera.cs
@@ -7,6 +7,9 @@ public class BasicCamera : MonoBehaviour
 
     public GameObject player;
     Vector3 offset;
+    Vector3 _velocity;
+    public float SmoothTime = 1;
+    public float MaxSpeed = 1;
 
 
     // Start is called before the first frame update
@@ -19,5 +22,8 @@ public class BasicCamera : MonoBehaviour
     void Update()
     {
         transform.position = player.transform.position + offset;
+
+        //transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + player.transform.forward * 2f + offset, ref _velocity, SmoothTime, MaxSpeed);
+
     }
 }
