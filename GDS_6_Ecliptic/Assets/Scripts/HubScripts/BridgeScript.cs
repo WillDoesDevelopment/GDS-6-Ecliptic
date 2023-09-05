e8b6ec9f55485995807afdbff500c34d2bc0fee3
PntherArt <PntherArt@gmail.com>
2023-09-05 15:42:09 +1000

    bridge sparkle,, nonsense

--- GDS_6_Ecliptic/Assets/Scripts/HubScripts/BridgeScript.cs
+++ GDS_6_Ecliptic/Assets/Scripts/HubScripts/BridgeScript.cs
@@ -8,6 +8,7 @@ public class BridgeScript : MonoBehaviour
     public GameObject bridge2;
     public GameObject openWall;
     public GameObject door;
+    public GameObject psStars;
     Renderer rend1;
     Renderer rend2;
 
@@ -22,6 +23,7 @@ public class BridgeScript : MonoBehaviour
     {
         bridge1.SetActive(false);
         bridge2.SetActive(false);
+        psStars.SetActive(false);
         openWall.SetActive(true);
         rend1 = bridge1.GetComponent<Renderer>();
         rend2 = bridge2.GetComponent<Renderer>();
@@ -45,15 +47,21 @@ public class BridgeScript : MonoBehaviour
         Debug.Log("connected");
         bridge1.SetActive(true);
         bridge2.SetActive(true);
+        psStars.SetActive(true);
         openWall.SetActive(false);
 
         shaderTimer = transform.position.z-1;     //Z coordinate of start of bridge
         rend1.material.SetFloat("_Timer", shaderTimer);
         rend2.material.SetFloat("_Timer", shaderTimer);
+        shaderTimer = psStars.transform.position.z-1;
 
         totalLength = Vector3.Distance(bridge1.transform.position, door.transform.position + doorOffset);
         bridge2.transform.localScale = new Vector3(1, 1, totalLength - bridge1Length);
         bridge1.transform.LookAt(door.transform.position + doorOffset);
+
+        totalLength = Vector3.Distance(psStars.transform.position, door.transform.position - doorOffset);
+        psStars.transform.localScale = new Vector3(1, 1, totalLength + bridge1Length);
+        psStars.transform.LookAt(door.transform.position - doorOffset);
     }
 
     [ContextMenu("Disconnect")]
@@ -61,6 +69,7 @@ public class BridgeScript : MonoBehaviour
     {
         bridge1.SetActive(false);
         bridge2.SetActive(false);
+        psStars.SetActive(false);
         openWall.SetActive(true);
 
     }
