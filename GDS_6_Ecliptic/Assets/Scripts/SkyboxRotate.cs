1793fcf66218809dd4473326afbc28ece0fc11cb
PntherArt <PntherArt@gmail.com>
2023-09-07 22:13:53 +1000

    Fixed Aries + Libra

--- GDS_6_Ecliptic/Assets/Scripts/SkyboxRotate.cs
+++ GDS_6_Ecliptic/Assets/Scripts/SkyboxRotate.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class SkyboxRotate : MonoBehaviour
 {
-    public GameObject starhub;
+    //public GameObject starhub;
     public float rotSpeed = 2f;
-    public float starSpeed = 2f;
+    //public float starSpeed = 2f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +19,6 @@ public class SkyboxRotate : MonoBehaviour
         //float offset = Mathf.Lerp(-180, 180, 0) / ;
         //starhub.transform.EulerAngles = new Vector3(0, transform.position.x * starSpeed, 0);
 
-        //RenderSettings.skybox.SetFloat("_Rotation", transform.position.x * rotSpeed);
+        RenderSettings.skybox.SetFloat("_Rotation", transform.position.x * rotSpeed);
     }
 }
