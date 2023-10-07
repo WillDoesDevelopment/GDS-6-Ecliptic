02c0f5bb59d8b7fe4772cee8aa0e35accd2c9608
GylesGyles <gyles.whytcross@gmail.com>
2023-10-08 02:22:34 +1100

    Grapple update

--- GDS_6_Ecliptic/Assets/Scripts/Grapple3.cs
+++ GDS_6_Ecliptic/Assets/Scripts/Grapple3.cs
@@ -12,12 +12,15 @@ public class Grapple3 : MonoBehaviour
     public GameObject body;
     public GameObject pointObject;
     public float length = 8f;
-    public float fireSpeed = 10f;
+    public float fireSpeed = 20f;
     public float t = 1;
     public float ropeWidth = 0.15f;
     public float maxLength = 10f;
+    float scopeTimer = 0;
     float castLength = 0;
-    bool headAttatch = false;
+    bool casting = false;
+    bool castComplete = false;
+    bool retract = false;
     public LayerMask grappleLayer;
 
     Transform playerPoint;
@@ -41,44 +44,70 @@ public class Grapple3 : MonoBehaviour
     {
 
         //Cast
-        if (target == null)
+        if (target == null && casting == false)
         {
             if (Input.GetMouseButton(0) ^ Input.GetKey(KeyCode.JoystickButton1) ^ Input.GetKey(KeyCode.Return))    //Draw indicator
             {
                 scope.SetActive(true);
-                scope.transform.position = (transform.position + transform.forward * maxLength / 2.0f);
-                scope.transform.LookAt(transform.position);
+                scope.transform.position = transform.position;                
+                scope.transform.rotation = transform.rotation;
+
+                scopeTimer += Time.deltaTime;
+
+                for (int i = 0; i < scope.transform.childCount; i++)                                                //Display arrows sequentially
+                {                    
+                    scope.transform.GetChild(i).gameObject.SetActive(scopeTimer * 20f > i);                    
+                }
+
             }
             else
             {
                 scope.SetActive(false);
+                scopeTimer = 0;
             }
 
 
             if (Input.GetMouseButtonUp(0) ^ Input.GetKeyUp(KeyCode.JoystickButton1) ^ Input.GetKeyUp(KeyCode.Return))  //Cast on release
-            {
-                var rayPos = transform.position;// - transform.forward;
+            {                
+                casting = true;
+                head.SetActive(true);
+                body.SetActive(true);
+                var rayPos = transform.position;
                 RaycastHit hit;
-                Debug.DrawLine(rayPos, rayPos + transform.forward * maxLength, Color.blue, 0.5f);                   //Draw debug line for cast
+                //Debug.DrawLine(rayPos, rayPos + transform.forward * maxLength, Color.blue, 0.5f);                   //Draw debug line for cast
 
-                if (Physics.Raycast(new Ray(rayPos, transform.forward), out hit, maxLength, grappleLayer))         //Raycast forward
+                if (Physics.Raycast(new Ray(rayPos, transform.forward), out hit, maxLength))                        //Raycast forward
+                {                    
+                    if (grappleLayer == (grappleLayer | (1 << hit.transform.gameObject.layer)))
+                    {
+                        //Succesfully hit grappleable object
+                        Debug.Log("grapple object hit");
+
+                        //Debug.DrawLine(hit.point, hit.point + transform.up * 2f, Color.green, 0.5f);                    //Draw debug line on hit
+                        target = hit.transform.gameObject;                                                              //Set target object                    
+                        pointObject.transform.position = hit.point;
+                        pointObject.transform.parent = target.transform;
+                        //rope.SetActive(true);
+                        
+                    }
+                    else
+                    {
+                        //Hit other object
+                        Debug.Log("other object hit");
+                        pointObject.transform.position = hit.point;
+                    }    
+                    
+                }
+                else
                 {
-                    Debug.DrawLine(hit.point, hit.point + transform.up * 2f, Color.green, 0.5f);                    //Draw debug line on hit
-                    target = hit.transform.gameObject;                                                              //Set target object                    
-                    pointObject.transform.position = hit.point;
-                    pointObject.transform.parent = target.transform;
-                    //rope.SetActive(true);
-                    head.SetActive(true);
-                    body.SetActive(true);
+                    //Hit no object
+                    Debug.Log("no object hit");
+                    pointObject.transform.position = rayPos + transform.forward * maxLength;
                 }
 
             }
 
-            
-
         }
-
-
 
         //Release
         if (Input.GetMouseButtonDown(1) ^ Input.GetKeyDown(KeyCode.JoystickButton0) ^ Input.GetKeyDown(KeyCode.RightShift))
@@ -86,26 +115,18 @@ public class Grapple3 : MonoBehaviour
             RopeBreak();
         }
 
+        //Casting
+        playerPoint = transform;
+        grapplePoint = pointObject.transform.position;
+        length = Vector3.Distance(playerPoint.position, grapplePoint);
 
+        if (casting == true)
+        {            
+            
 
-
-
-        //Object follow
-
-        if (target != null)
-        {
-            playerPoint = transform;
-            grapplePoint = pointObject.transform.position;
-            length = Vector3.Distance(playerPoint.position, grapplePoint);
-
-            //Target
-            playerXZ = new Vector3(transform.position.x, target.transform.position.y, transform.position.z);
-            t = length / maxLength;                                                             // % of max length extended
-
-
-            //Throw animation
-            if(headAttatch == false)
+            if (castComplete == false)
             {
+                //Stretch out
                 head.transform.position = Vector3.Lerp(playerPoint.position, grapplePoint, (castLength / length) - 0.01f);
                 head.transform.LookAt(grapplePoint);
 
@@ -113,45 +134,86 @@ public class Grapple3 : MonoBehaviour
                 body.transform.localScale = new Vector3(1f, 1f, castLength);
                 body.transform.LookAt(grapplePoint);
 
-                castLength += Time.deltaTime * fireSpeed;                
+                castLength += Time.deltaTime * fireSpeed;
 
                 if (castLength > length)
                 {
                     castLength = length;
-                    headAttatch = true;
-                    castLength = 0;
+                    castComplete = true;                    
                 }
             }
-            //Move snake
-            else
+            else if(target == null)
             {
-                head.transform.position = Vector3.Lerp(playerPoint.position, grapplePoint, 0.99f);
+                //Return
+                head.transform.position = Vector3.Lerp(playerPoint.position, grapplePoint, (castLength / length) - 0.01f);
                 head.transform.LookAt(grapplePoint);
 
-                body.transform.position = (playerPoint.position + grapplePoint) / 2.0f;
-                body.transform.localScale = new Vector3(1f, 1f, length);
+                body.transform.position = Vector3.Lerp(playerPoint.position, grapplePoint, (castLength / length / 2f) - 0.01f); ;
+                body.transform.localScale = new Vector3(1f, 1f, castLength);
                 body.transform.LookAt(grapplePoint);
+
+                castLength -= Time.deltaTime * fireSpeed;
+
+                if (castLength < 0f)
+                {                    
+                    casting = false;
+                    castComplete = false;
+                    castLength = 0;
+                    head.SetActive(false);
+                    body.SetActive(false);
+                }
             }
-            
-            
-            //Force
+            else
+            {
+                //Attatch and Pull Object
+                AddForce();
+            }
+        }
 
-            float f = Mathf.Clamp(t - 1f, 0, 0.2f) * 5f;                                       // force modifier when 1.0 < t < 1.2
+        //Retracting
+        if (retract)
+        {
+            head.transform.position = Vector3.Lerp(playerPoint.position, grapplePoint, (castLength / length) - 0.01f);
+            head.transform.LookAt(grapplePoint);
 
-            var forceVec = Vector3.Normalize(playerXZ - target.transform.position) * Time.deltaTime * f * 100000f;    //Calculate force            
-            target.GetComponent<Rigidbody>().AddForceAtPosition(forceVec, pointObject.transform.position);            //Add force
+            body.transform.position = Vector3.Lerp(playerPoint.position, grapplePoint, (castLength / length / 2f) - 0.01f); ;
+            body.transform.localScale = new Vector3(1f, 1f, castLength);
+            body.transform.LookAt(grapplePoint);
 
-            //Rope
-            /*
-            rope.transform.position = (playerPoint.position + grapplePoint) / 2.0f;
-            rope.transform.localScale = new Vector3(ropeWidth, ropeWidth, length);
-            rope.transform.LookAt(grapplePoint);
-            
+            castLength -= Time.deltaTime * fireSpeed;
 
-            rend.material.SetFloat("_Stretch_Amount", Mathf.Clamp(t, 0, 1));
-            */
+            if (castLength < 0f)
+            {
+                //Reset grapple
+                target = null;
+                retract = false;
+                casting = false;
+                castComplete = false;
+                castLength = 0;
+                head.SetActive(false);
+                body.SetActive(false);
+            }
+        }
 
-            //column interaction
+        //Grapple Follow
+        if (target != null && castComplete == true)
+        {
+            head.transform.position = Vector3.Lerp(playerPoint.position, grapplePoint, 0.99f);
+            head.transform.LookAt(grapplePoint);
+
+            body.transform.position = (playerPoint.position + grapplePoint) / 2.0f;
+            body.transform.localScale = new Vector3(1f, 1f, length);
+            body.transform.LookAt(grapplePoint);
+        }
+
+
+
+
+
+        //column interaction
+
+        if (target != null)
+        {                      
             if (target.GetComponent<columnScript>() != null)
             {
                 //Debug.Log("almost");
@@ -168,19 +230,22 @@ public class Grapple3 : MonoBehaviour
                 RopeBreak();
             }
         }
+    }
 
+    void AddForce()
+    {
+        t = length / maxLength;                                                             // % of max length extended
+        playerXZ = new Vector3(transform.position.x, target.transform.position.y, transform.position.z);
+        float f = Mathf.Clamp(t - 1f, 0, 0.2f) * 5f;                                       // force modifier when 1.0 < t < 1.2
 
-
-
+        var forceVec = Vector3.Normalize(playerXZ - target.transform.position) * Time.deltaTime * f * 100000f;    //Calculate force            
+        target.GetComponent<Rigidbody>().AddForceAtPosition(forceVec, pointObject.transform.position);            //Add force
     }
 
     public void RopeBreak()
-    {
-        target = null;
-        //rope.SetActive(false);        
-        head.SetActive(false);
-        body.SetActive(false);
-        headAttatch = false;
-        castLength = 0;
+    {        
+        castComplete = false;
+        casting = false;
+        retract = true;             //Start retracting
     }
 }
