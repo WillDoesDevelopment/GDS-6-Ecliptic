5f537b5ba2a69db61d012fee88eecf77ce8e107d
GylesGyles <gyles.whytcross@gmail.com>
2023-10-11 18:01:21 +1100

    Scorpio walking sound

---
+++ GDS_6_Ecliptic/Assets/Scripts/ScorpioScripts/ScorpianWalk.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorpianWalk : MonoBehaviour
+{
+    public GameObject player;
+    public AudioSource AudioSource;
+    Vector3 pastPosition;
+    Vector3[] Positions = new Vector3[4];
+    int i = 0;
+
+    float timer = 1f;
+    bool pause = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pastPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if(timer <0)
+        {
+            timer += 0.5f;
+
+            if (transform.position != pastPosition)
+            {
+                
+                if(pause && Vector3.Distance(transform.position, player.transform.position) < 10)
+                {
+                    Debug.Log("sfx play");
+                    AudioSource.Play();
+                    pause = false;
+                }
+                
+                
+                pastPosition = transform.position;
+            }            
+            else if(!pause)
+            {
+                pause = true;
+                AudioSource.Pause();
+                Debug.Log("sfx pause");
+            }
+
+        }
+
+
+        
+    }
+}
