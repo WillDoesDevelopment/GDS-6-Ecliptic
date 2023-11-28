1407c032e6e4967eaf6b89904a1ac6811761755d
WillDoesDevelopment <s3901070@student.rmit.edu.au>
2023-11-28 13:51:40 +1100

    Fixed sheep sounds and put stairs into position

--- GDS_6_Ecliptic/Assets/Scripts/AriesScripts/GoldSheepScript.cs
+++ GDS_6_Ecliptic/Assets/Scripts/AriesScripts/GoldSheepScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GoldSheepScript : MonoBehaviour
 {
     public Animator PlayerAnim;
+    public Animator SheepMovmentAnim;
     bool canStep = true;
     [Range(0f, 1f)] public float stepVolume = 0.8f;
     [Range(0f, 0.8f)] public float stepPitchVariance = 0.5f;
@@ -20,6 +21,7 @@ public class GoldSheepScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
 
        if(transform.GetChild(0).GetComponent<Animator>().GetBool("Animate") == true)
         {
@@ -56,6 +58,7 @@ public class GoldSheepScript : MonoBehaviour
     {
         transform.GetChild(0).GetComponent<Animator>().SetTrigger("Animate2");
         canStep = false;
+        audioSource.enabled = false;
     }
 
     void Footstep()
