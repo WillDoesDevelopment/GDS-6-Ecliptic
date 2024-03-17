9b455d2bf71db72bf035468d1fd36d7d2ab6d6aa
GylesGyles <gyles.whytcross@gmail.com>
2024-03-17 23:57:10 +1100

    Gemini prototyping

---
+++ GDS_6_Ecliptic/Assets/Scripts/Gemini Scripts/linearOscilation.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class linearOscilation : MonoBehaviour
+{
+    public Transform point1;
+    public Transform point2;
+    //public Transform mirroredObject;
+    public AnimationCurve curve;                    //input curve in editor    
+    public float travelTime = 5;
+    float timer = 0;
+    float a = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime * a;
+
+        if(timer > travelTime)
+        {
+            timer = travelTime + travelTime - timer;    //any extra time is carried over
+            a = -a;                                     //reverse direction
+        }
+        if (timer < 0f)
+        {
+            timer = -timer;                             //any extra time is carried over
+            a = -a;                                     //reverse direction
+        }
+        timer = Mathf.Clamp(timer, 0, travelTime);      //clamp just in case
+
+        transform.position = Vector3.Lerp(point1.position, point2.position, curve.Evaluate(timer / travelTime));    //lerp between points based on timer input to animation curve
+    }
+}
