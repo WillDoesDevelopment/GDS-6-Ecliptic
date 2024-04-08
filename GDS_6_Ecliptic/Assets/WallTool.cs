2644ff8a1cf3a1453b008cdc7072b780212d8c71
PntherArt <PntherArt@gmail.com>
2024-04-08 15:44:25 +1000

    I CAN MAKE COLUMNS

--- GDS_6_Ecliptic/Assets/WallTool.cs
+++ GDS_6_Ecliptic/Assets/WallTool.cs
@@ -1,9 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
-public class WallTool : Editor
+public class WallTool : MonoBehaviour
 {
+    private Vector3 mPos;
+    private Vector3 objPos;
 
+    public GameObject wall;
+    public GameObject column;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mPos = Input.mousePosition;
+            mPos.z = 25.0f;
+
+            objPos = Camera.main.ScreenToWorldPoint(mPos);
+            Instantiate(column, objPos, Quaternion.identity);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            mPos = Input.mousePosition;
+            mPos.z = 25.0f;
+
+            objPos = Camera.main.ScreenToWorldPoint(mPos);
+            Instantiate(column, objPos, Quaternion.identity);
+        }
+    }
 }
