7759c3051964c7daee32f985d3ddf0ef9c93b1de
WillDoesDevelopment <s3901070@student.rmit.edu.au>
2024-04-17 17:10:53 +1000

    Trying an new method for branching dialogue
    instead of indenting every line accept for dialogue options I only indent dialogue options and its corresponding dialogue

--- GDS_6_Ecliptic/Assets/Scripts/DialogueScripts/Dialogue.cs
+++ GDS_6_Ecliptic/Assets/Scripts/DialogueScripts/Dialogue.cs
@@ -7,42 +7,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "data/dialogue")]
 public class Dialogue: ScriptableObject
 {
-    // setting mode to not started
-    public Dialogue()
-    {
-        DialogueMode = DialogueState.NotStarted;
-        //formatDialogueLine()
-    }
-
-
-    [System.Serializable]
-    public struct DialogueLine 
-    { 
-        // all the variables per line of dialogue
-        public string sentence; 
-        public DialogueType dialogueType;
-        public AudioClip AS;
-        public int OrderAndChaosVal;
-        
-        //public int OtherSpeaker;
-    }
-
-    // this is the origonal dialogue list
-    public DialogueLine[] line; 
-    public void formatDialogueLine(ref DialogueLine[] DL)
-    {
-        for (int i = 0; i<DL.Length; i++)
-        {
-            for(int s = 0; s < i; s++)
-            {
-                string sentence = DL[i].sentence;
-                DL[i].sentence = " " + sentence;
-
-            }
-        }
-    }
-
-    // all required information for the dialogue
+    public DialogueLine[] line;
     public enum DialogueType
     {
         OtherDialogue,
@@ -66,9 +31,130 @@ public class Dialogue: ScriptableObject
     }
     //[System.NonSerialized]
     public DialogueState DialogueMode = DialogueState.NotStarted;
-
-    private void OnEnable()
+    // This is a constructor
+    public Dialogue()
     {
         DialogueMode = DialogueState.NotStarted;
+        //formatDialogueLine(line);
+    }
+    [System.Serializable]
+    public struct DialogueLine 
+    { 
+        // all the variables per line of dialogue
+        public string sentence; 
+        public DialogueType dialogueType;
+        public AudioClip AS;
+        //public bool newDecision;
+        //public bool IsDecision;
+        //public int OrderAndChaosVal;
+        
+        //public int OtherSpeaker;
+    }
+    
+
+    // this indents all the lins in our sentence 
+    public void formatDialogueLine(ref DialogueLine[] DL)
+    {
+       //goes through every sentence in array
+        for (int i = 0; i<DL.Length; i++) 
+        {
+            /* 
+            //since the sentences are saved to memory permanently we need to use the trim function to get rid of the blank spaces in the data each time it runs
+            DL[i].sentence = DL[i].sentence.Trim();
+
+
+            // counter to keep trck of amount of spaces
+            int indentCounter = 0;
+
+
+            //if this is our first array entry, add one space
+            if (i == 0) 
+            {
+                //DL[i].sentence = DL[i].sentence.Trim();
+                string Tempsentence = DL[i].sentence;
+                DL[i].sentence = " " + Tempsentence;
+            }
+
+            // else we check the characters in the previous sentence and add a value to the indent counter 
+            else if (DL[i].IsDecision)
+            {
+                for (int k = i; k > 0; k--)
+                {
+                    if (DL[k].newDecision == true)
+                    {
+                        //Debug.Log(k + "is new decision");
+                        foreach (char c in DL[k].sentence)
+                        {
+                            if (c == ' ')
+                            {
+                                indentCounter++;
+                            }
+
+                            for (int y = 0; y <= indentCounter; y++)
+                            {
+                                DL[i].sentence = DL[i].sentence.Trim();
+                                string sentence = DL[i].sentence;
+                                DL[i].sentence = " " + sentence;
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            else
+            {
+                foreach (char c in DL[i - 1].sentence)
+                {
+                    if (c == ' ')
+                    {
+                        indentCounter++;
+                    }
+                    else
+                    {
+                        //Debug.Log(indentCounter);
+                        break;
+                    }
+
+                }
+                for (int k = 0; k <= indentCounter; k++)
+                {
+                    //Debug.Log("ran" + k + "times");
+                    string sentence = DL[i].sentence;
+                    DL[i].sentence = " " + sentence;
+                }
+
+            }
+            
+           *//* for(int s = 0; s < i; s++)
+            {
+                string Tempsentence = DL[i].sentence;
+                DL[i].sentence = " " + Tempsentence;
+
+            }*//*
+            // this code is in here to make the sentances display withought indentation
+            */
+            //DL[i].sentence = DL[i].sentence.Trim();
+            //Debug.Log(DL[i].sentence.Trim());
+        }   
+    }
+    public void DialogueDecisionCheck(ref DialogueLine [] DL)
+    {
+        foreach(DialogueLine Dialogue in DL )
+        {
+
+        }
+    }
+
+    // all required information for the dialogue
+
+    private void OnEnable() 
+    {
+        DialogueMode = DialogueState.NotStarted;
+    }
+    private void OnValidate()
+    {
+        formatDialogueLine(ref line);
+        
     }
 }
