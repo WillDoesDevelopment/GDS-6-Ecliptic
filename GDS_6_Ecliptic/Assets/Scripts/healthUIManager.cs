779a0c989b9de5187ed034ef26bb77a1fa332e69
WillDoesDevelopment <s3901070@student.rmit.edu.au>
2024-07-21 18:39:32 +1000

    health UI works in Leo scene

---
+++ GDS_6_Ecliptic/Assets/Scripts/healthUIManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthUIManager : MonoBehaviour
+{
+    public PlayerController PC;
+    private int tempHealth;
+    public GameObject[] HealthUIList;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        PC = GameObject.FindObjectOfType<PlayerController>();
+        HealthUIDisplay();
+        tempHealth = PC.health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(tempHealth!= PC.health)
+        {
+            HealthUIDisplay();
+            Debug.Log(PC.health);
+            tempHealth = PC.health;
+        }
+    }
+
+    public void HealthUIDisplay()
+    {
+        Debug.Log("running");
+        foreach(GameObject g in HealthUIList)
+        {
+            g.SetActive(false);
+        }
+        for (int i = 0; i < PC.health; i++)
+        {
+            HealthUIList[i].SetActive(true);
+            Debug.Log(i);
+        }
+    }
+}
