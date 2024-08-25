45f4114c1d2ee2fbaaac2546b15e2577ca7687dd
Tj <109777815+PntherArt@users.noreply.github.com>
2024-08-25 17:26:26 +1000

    gemin updates!

---
+++ GDS_6_Ecliptic/Assets/BridgeTriggerG.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BridgeTriggerG : MonoBehaviour
+{
+    float timeElapsed;
+    public float lerpDuration = 5;
+
+    public Renderer rend1;
+    public Renderer rend2;
+    float shaderTimer = 110;
+    bool isColliding = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if(isColliding == true)
+        {
+            shaderTimer = Mathf.Lerp(110, 170, timeElapsed / lerpDuration);
+            timeElapsed += Time.deltaTime;
+        }
+
+        rend1.material.SetFloat("_Timer", shaderTimer);
+        rend2.material.SetFloat("_Timer", shaderTimer);
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        isColliding = true;
+    }
+   
+
+}
