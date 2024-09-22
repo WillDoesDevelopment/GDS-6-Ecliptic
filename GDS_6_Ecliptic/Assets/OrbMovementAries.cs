966c0b9d46eda28d931279417375128d98e36553
Tj <109777815+PntherArt@users.noreply.github.com>
2024-09-22 17:46:44 +1000

    Setting aries orb up for movement in scene

--- GDS_6_Ecliptic/Assets/OrbMovementAries.cs
+++ GDS_6_Ecliptic/Assets/OrbMovementAries.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class OrbMovementAries : MonoBehaviour
 {
+    public Animator Anim;
+    public Transform[] Points;
+    public float speed;
+    public int pointsIndex;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = Points[pointsIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(pointsIndex <= Points.Length - 1)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, Points[pointsIndex].transform.position, speed * Time.deltaTime);
+            
+            if(transform.position == Points[pointsIndex].transform.position )
+            {
+                pointsIndex++;
+                Anim.SetBool("isMoving", true);
+            }
+
+            if(pointsIndex == Points.Length)
+            {
+                pointsIndex = 0;
+                Anim.SetBool("isMoving", false);
+            }
+        }
     }
 }
