3e27f163e553bb15f5721a34548872ea540df266
Tj <109777815+PntherArt@users.noreply.github.com>
2024-09-26 12:27:10 +1000

    finished 1-3 gemini beats!

--- GDS_6_Ecliptic/Assets/Scripts/Gemini Scripts/geminibeats.cs
+++ GDS_6_Ecliptic/Assets/Scripts/Gemini Scripts/geminibeats.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class geminibeats : MonoBehaviour
 {
     public GameObject Ophieno2;
     public DialogueTrigger DT;
+    public Dialogue prevDia;
+    public BoxCollider BC;
+    public VFXCircleHandler VFXCH;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        BC = GetComponent<BoxCollider>();
+        VFXCH.circleVFXStart();
+
+    }
+
+    private void Update()
+    {
+        if (prevDia.DialogueMode == Dialogue.DialogueState.Finished)
+        {
+            BC.isTrigger = true;
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (DT.OnEvent == true)
+        if (DT.OnEvent == true && BC.isTrigger == true)
         {
             DT.OnEventCheck();
             Ophieno2.SetActive(true);
