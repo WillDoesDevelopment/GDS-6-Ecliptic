238162ebfd9450d2d9a9dc53cf02393728b89c97
WillDoesDevelopment <s3901070@student.rmit.edu.au>
2025-01-20 21:40:19 +1100

    Lots of changes, Taurus plays most of the way through now
    pretty happy with the code

--- GDS_6_Ecliptic/Assets/Scripts/TaurusPuzzle/New Taurus Manager Scripts/NewTaurusManager.cs
+++ GDS_6_Ecliptic/Assets/Scripts/TaurusPuzzle/New Taurus Manager Scripts/NewTaurusManager.cs
@@ -6,6 +6,7 @@ public abstract class TaurusStage : MonoBehaviour
 {
     public GameObject bull;
     public GameObject Artifact;
+    public GameObject TargetObject;
 
 
     public Vector3 PlayerResetPos;
@@ -20,9 +21,16 @@ public abstract class TaurusStage : MonoBehaviour
     {
         if (Vector3.Distance(Player.transform.position, bull.transform.position) < 1f)
         {
+
+            Player.GetComponent<DialogueTrigger>().OnEventCheck();
+            Player.GetComponent<DialogueTrigger>().OnEvent = false;
+
+            //this telliports the player without being over written by the character controller
             Player.GetComponent<CharacterController>().enabled = false;
             Player.transform.position = PlayerResetPos;
             Player.GetComponent<CharacterController>().enabled = true;
+
+
             Debug.Log(Player.transform.position);
 
             Artifact.transform.position = ArtifactResetPos;
@@ -31,9 +39,42 @@ public abstract class TaurusStage : MonoBehaviour
         }
     }
 
-    public void CollectArtifact()
+    public bool ArtifactCheck(GameObject Player, GameObject particleObject)
     {
-
+        GameObject HO = Player.GetComponent<PickUpScript>().HoldingObj;
+        if (HO == null)
+        {
+            return false;
+        }
+        if (!HO.CompareTag("PickUp"))
+        {
+            particleObject.SetActive(false);
+        }
+        else
+        {
+            particleObject.SetActive(true);
+            particleObject.transform.position = HO.transform.position;
+            particleObject.transform.LookAt(transform.position);
+            
+        }
+        if (HO.GetComponent<Artifact>() != null && Vector3.Distance(TargetObject.transform.position, HO.transform.position) < 3)
+        {
+            TargetObject.transform.GetChild(0).GetComponent<DoorScript>().DS.IsOpen = true;
+            HO.transform.GetChild(0).GetComponent<ParticleSystem>().Play();
+            HO.transform.DetachChildren();
+/*            CollectedItems += 1;
+            if (CollectedItems <= 3)
+            {
+                GoodJobSND.GetComponent<AudioSource>().Play();
+            }*/
+            Player.GetComponent<PickUpScript>().holding = false;
+            //HO.gameObject.SetActive(false);
+            HO.SetActive(false);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public void StageWinCondition()
@@ -59,10 +100,12 @@ public abstract class TaurusStage : MonoBehaviour
 
 public class NewTaurusManager : MonoBehaviour
 {
+
     public TaurusStage[] TaurusStages;
     public int taurusStageCounter;
 
     public GameObject Player;
+    public GameObject particleObject;
 
     private void Start()
     {
@@ -72,6 +115,11 @@ public class NewTaurusManager : MonoBehaviour
     public void Update()
     {
         TaurusStages[taurusStageCounter].ResetCheck(Player);
+        if (TaurusStages[taurusStageCounter].ArtifactCheck(Player, particleObject))
+        {
+            taurusStageCounter += 1;
+        }
+
     }
 
     
