be8b12a244dc184dbb28a98c00c44ce88fae8c50
Tj <109777815+PntherArt@users.noreply.github.com>
2025-01-23 20:25:01 +1100

    attempted to get trigger dissolve to work,, mehhh

--- GDS_6_Ecliptic/Assets/Scripts/LeoScripts/columnScript.cs
+++ GDS_6_Ecliptic/Assets/Scripts/LeoScripts/columnScript.cs
@@ -12,6 +12,10 @@ public class columnScript : MonoBehaviour
     public GameObject lion;
     public GameObject Shield;
 
+    public Material mat;
+    private float alphaFloat = 0f;
+    private float change = 0.2f;
+
     float timer = 0f;
 
     bool stage1Lock = false;
@@ -20,7 +24,9 @@ public class columnScript : MonoBehaviour
 
     void Start()
     {
-
+        alphaFloat = 0f;
+        mat.GetFloat("_Clip_Amt");
+        mat.SetFloat("_Clip_Amt", alphaFloat);
     }
 
     // Update is called once per frame
@@ -29,12 +35,16 @@ public class columnScript : MonoBehaviour
         if (fall == true)
         {
             FallUpdate();
+            InvokeRepeating("DissolveTrigger", 0.5f, 10f);
         }
 
         if (fall == false && lion.GetComponent<lionAI>().stage == 2)
         {
             Shield.SetActive(true);
+            
+
         }
+
 
         /*
         if (Input.GetKeyDown(KeyCode.P))
@@ -47,6 +57,9 @@ public class columnScript : MonoBehaviour
             fall = true;
         }
         */
+
+
+
     }
 
     public void FallUpdate()
@@ -57,18 +70,20 @@ public class columnScript : MonoBehaviour
         {
             BreakStage1();
             stage1Lock = true;
+            
         }
 
-        if (timer > 4 && stage2Lock == false)
+        if (timer > 8 && stage2Lock == false)
         {
             BreakStage2();
             stage2Lock = true;
         }
 
-        if (timer > 4 && stage3Lock == false)
+        if (timer > 8 && stage3Lock == false)
         {
             BreakStage3();
             stage3Lock = true;
+
         }
     }
 
@@ -86,10 +101,20 @@ public class columnScript : MonoBehaviour
         brokenColumn.SetActive(false);
         barrierObject.GetComponent<barrierSegment>().move = true;
         gameObject.SetActive(false);
+
     }
 
     void BreakStage3()
     {
               
     }
+
+    void DissolveTrigger()
+    {
+        mat.GetFloat("_Clip_Amt");
+
+        alphaFloat += change;
+        mat.SetFloat("_Clip_Amt", alphaFloat);
+    }
+
 }
