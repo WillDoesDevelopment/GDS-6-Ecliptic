f56c5e6aa28cf82d5d359a17d74fbb9f3a953805
Tj <109777815+PntherArt@users.noreply.github.com>
2025-02-06 20:42:32 +1100

    setting up vfx floating blocks

---
+++ GDS_6_Ecliptic/Assets/SpikeCubeSpin.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeCubeSpin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.deltaTime;
+        //transform.eulerAngles = Mathf.Lerp(0, 360, t);
+
+
+    }
+}
