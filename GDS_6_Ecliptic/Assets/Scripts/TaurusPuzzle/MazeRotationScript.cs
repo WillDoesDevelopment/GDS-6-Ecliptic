e4d8496c44fc7af3dd440308ca95a599741e87de
Tj <109777815+PntherArt@users.noreply.github.com>
2025-06-05 20:16:47 +1000

    taurus bugs

--- GDS_6_Ecliptic/Assets/Scripts/TaurusPuzzle/MazeRotationScript.cs
+++ GDS_6_Ecliptic/Assets/Scripts/TaurusPuzzle/MazeRotationScript.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class MazeRotationScript : MonoBehaviour
 {
     public float rotSpeed;
+
+    public DoorScript DS;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //DS = GetComponent<DoorStatus>().IsOpen;
     }
 
     // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
+        /*
+        if (DS.IsOpen == true)
+        {
+            
+            print("DOING THE THING");
+        }*/
+
         this.transform.eulerAngles += new Vector3(0, rotSpeed, 0);
+
     }
+
 }
