23b247faaf489b87258cb2d07e180cad0e73f8df
GylesGyles <gyles.whytcross@gmail.com>
2025-08-01 16:52:42 +1000

    Screenshot function for character controller

--- GDS_6_Ecliptic/Assets/Scripts/gameIEnums.cs
+++ GDS_6_Ecliptic/Assets/Scripts/gameIEnums.cs
@@ -7,10 +7,20 @@ public class gameIEnums : MonoBehaviour
     public static IEnumerator ScreenshotIEnum()
     {
         yield return new WaitForEndOfFrame();
-        string m_Path = Application.dataPath;
-        string screenshotFile = m_Path + "/Screenshots/" + "Ecliptic_Screenshot" + System.DateTime.Now.ToString("MM-dd-yy (HH-mm-ss)") + ".png";
-        ScreenCapture.CaptureScreenshot(screenshotFile,4);
+        string m_Path = Application.dataPath +"/Screenshots";        
+
+        bool exists = System.IO.Directory.Exists(m_Path);
+
+        if (!exists)
+            System.IO.Directory.CreateDirectory(m_Path);
+
+        string screenshotFile = m_Path + "/Ecliptic_Screenshot" + System.DateTime.Now.ToString("MM-dd-yy (HH-mm-ss)") + ".png";
+        int imageSizeMultiplier = 4;
+
+        ScreenCapture.CaptureScreenshot(screenshotFile, imageSizeMultiplier);
+
         Debug.Log("A screenshot was taken!"+ "\n" + screenshotFile);
+
         //StartCoroutine(screenshotScript.ScreenshotIEnum());
     }
 
