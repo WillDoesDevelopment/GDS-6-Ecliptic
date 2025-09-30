a79bf21d1972956e14955c94be54327cec4655f3
Tj <109777815+PntherArt@users.noreply.github.com>
2025-09-30 12:57:11 +1000

    door material update
    - updated doors to change material to lit when open
    - currently does not change when door open bool is triggered by only when the door is animated open

--- GDS_6_Ecliptic/Assets/Scripts/HubScripts/DoorScript.cs
+++ GDS_6_Ecliptic/Assets/Scripts/HubScripts/DoorScript.cs
@@ -17,6 +17,9 @@ public class DoorScript : MonoBehaviour
     public int StageNumber;
     public bool BackToHub;
 
+    public Renderer rend;
+    public Material[] mat;
+
     //public AudioSource DoorSnd;
     
     // at the moment each door script needs to know about the hub manager
@@ -38,16 +41,26 @@ public class DoorScript : MonoBehaviour
         DoorEnteredCheck();
 
     }
+
+    private void FixedUpdate()
+    {
+        if(DS.IsOpen == true)
+        {
+            rend.material = mat[0];
+        }
+    }
     public void DoorOpenCheck()
     {
         if (proximity(Radius) && DS.IsOpen == true)
         {
-            ThisAnim.SetBool("Animate", true);
+            rend.material = mat[0];
+            ThisAnim.SetBool("Animate", true); 
 
         }
         else
         {
             ThisAnim.SetBool("Animate", false);
+            rend.material = mat[1];
         }
     }
     public void DoorEnteredCheck()
