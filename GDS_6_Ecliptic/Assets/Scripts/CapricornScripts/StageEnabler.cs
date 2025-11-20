7a17b3a62c99bc2999a665e24412e92aedc2167c
GylesGyles <gyles.whytcross@gmail.com>
2025-11-21 00:01:46 +1100

    Capricorn room staging setup

---
+++ GDS_6_Ecliptic/Assets/Scripts/CapricornScripts/StageEnabler.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageEnabler : MonoBehaviour
+{
+    public CapStage targetCapStage;
+
+    [ContextMenu("Update Capricorn Stage")]
+    void ShowCapStage()
+    {
+        foreach (Transform child in transform)
+        {            
+            //Debug.Log(child.name);
+            
+            if (child.gameObject.TryGetComponent<CapStageHolder>(out CapStageHolder capStageHolder))
+            {
+                if((capStageHolder.capStage & targetCapStage) != 0)
+                {
+                    child.gameObject.SetActive(true);
+                }
+                else
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }            
+        }
+    }
+
+    [ContextMenu("Enable All Children")]
+    void EnableAllChildren()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+    }
+}
