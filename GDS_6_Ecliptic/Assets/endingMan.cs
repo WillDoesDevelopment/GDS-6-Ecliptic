c1669ac279d7ab25a6953a80363c61ccc61725a9
Tj <109777815+PntherArt@users.noreply.github.com>
2025-11-30 18:55:11 +1100

    further updates
    - constellation route set up
    - ending level check active

--- GDS_6_Ecliptic/Assets/endingMan.cs
+++ GDS_6_Ecliptic/Assets/endingMan.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using static Cinemachine.DocumentationSortingAttribute;
@@ -11,6 +12,9 @@ public class endingMan : MonoBehaviour
     public GameObject[] endObj;
     public Dialogue[] dia;
     public PlayerController pc;
+    public HubManager hm;
+    public bool endStarted = false;
+    private bool globDiaAct = false;
 
     [Header("Human Ending")]
     public string HumanScene;
@@ -20,51 +24,72 @@ public class endingMan : MonoBehaviour
     public GameObject hub;
     public GameObject globe;
 
+    [ColorUsage(true, true)]
     public Color colour;
     public Material newDoorMat;
 
     public GameObject globeDia;
 
-    void Start()
-    {
-        endObj[0].SetActive(true);
-        endObj[1].SetActive(true);
-    }
-
     // Update is called once per frame
     void Update()
     {
-        if (dia[1].DialogueMode == Dialogue.DialogueState.Finished) //when the chatter has stopped, spawn in both constellation and symbol to choose
+        if(HubManager.LevelNumber == 12 && globDiaAct == false)
+        {
+            StartCoroutine(endStartUp());
+            globeDia.SetActive(false);
+        }
+
+        if (dia[0].DialogueMode == Dialogue.DialogueState.Finished) //when the chatter has stopped, spawn in both constellation and symbol to choose
         {
             endObj[2].SetActive(true);
             endObj[3].SetActive(true);
+            
         }
 
-        if (dia[2].DialogueMode == Dialogue.DialogueState.InProgress) //Picking the constellation
+        if (dia[1].DialogueMode == Dialogue.DialogueState.InProgress) //Picking the constellation
         {
             endObj[3].SetActive(false); //symbol is hidden
 
-            if (dia[2].DialogueMode == Dialogue.DialogueState.Finished)
-            {
-                endObj[2].SetActive(false); //constellation is also hidden
-            }
         }
 
-        if (dia[3].DialogueMode == Dialogue.DialogueState.InProgress) //Picking the Earth Symbol
+        if (dia[1].DialogueMode == Dialogue.DialogueState.Finished && endStarted == false)
+        {
+            endObj[2].SetActive(false);
+            endObj[3].SetActive(false); //constellation is also hidden
+            globeDia.SetActive(true);
+            StartCoroutine(God());
+        }
+
+        if (dia[2].DialogueMode == Dialogue.DialogueState.InProgress) //Picking the Earth Symbol
         {
             endObj[2].SetActive(false); //constellation is hidden
+        }
 
-            if (dia[3].DialogueMode == Dialogue.DialogueState.Finished)
-            {
-                endObj[3].SetActive(false); //symbol is also hidden
-                StartCoroutine(Human()); //start human ending
-            }
+        if (dia[2].DialogueMode == Dialogue.DialogueState.Finished && endStarted == false)
+        {
+            endObj[2].SetActive(false);
+            endObj[3].SetActive(false); //symbol is also hidden
+            StartCoroutine(Human()); //start human ending
         }
 
     }
 
+
+    IEnumerator endStartUp()
+    {
+        endObj[0].SetActive(true);
+        endObj[1].SetActive(true);
+        pc.canWalk = false;
+        yield return new WaitForSeconds(3f);
+        endObj[4].SetActive(true);
+        pc.canWalk = true;
+        globDiaAct = true;
+    }
+
+
     IEnumerator Human()
     {
+        endStarted = true;
         pc.canWalk = false;
         yield return new WaitForSeconds(3f);
         SceneManager.LoadScene(HumanScene);
@@ -73,10 +98,14 @@ public class endingMan : MonoBehaviour
 
     IEnumerator God()
     {
+        endStarted = true;
         pc.canWalk = false;
         yield return new WaitForSeconds(3f);
-        hub.GetComponent<Material>().SetColor("_EmissionColor", colour);
-        globe.GetComponent<Material>().SetColor("_EmissionColor", colour);
+        hub.GetComponent<Renderer>().material.SetColor("_EmissionColor", colour);
+        globe.GetComponent<Renderer>().material.SetColor("_EmissionColor", colour);
+        yield return new WaitForSeconds(3f);
+        door.GetComponent<Renderer>().material = newDoorMat;
+        pc.canWalk = true;
 
     }
 
