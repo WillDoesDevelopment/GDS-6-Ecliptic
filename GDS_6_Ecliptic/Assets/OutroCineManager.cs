387945be6dd9f5f10e256407d80abb235893df6a
Tj <109777815+PntherArt@users.noreply.github.com>
2025-12-02 20:08:02 +1100

    human ending finished

--- GDS_6_Ecliptic/Assets/OutroCineManager.cs
+++ GDS_6_Ecliptic/Assets/OutroCineManager.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class OutroCineManager : MonoBehaviour
 {
+
+    public GameObject[] obj;
+    public DialogueTrigger dt;
+    public Animator anim;
+    public GameObject[] vcam;
+  
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(letsgo());
     }
 
     // Update is called once per frame
@@ -15,4 +22,23 @@ public class OutroCineManager : MonoBehaviour
     {
         
     }
+
+    IEnumerator letsgo()
+    {
+        yield return new WaitForSeconds(5f);
+        obj[0].SetActive(true);
+        anim.SetTrigger("go");
+        yield return new WaitForSeconds(3f);
+        vcam[0].SetActive(false);
+        vcam[1].SetActive(false);
+        obj[1].SetActive(false); //player
+        yield return new WaitForSeconds(10f);
+        obj[2].SetActive(true); //2d canvas
+        obj[3].SetActive(false); //ophistar
+        yield return new WaitForSeconds(5f);
+        obj[2].SetActive(false);
+        dt.OnEventCheck();
+
+    }
+
 }
