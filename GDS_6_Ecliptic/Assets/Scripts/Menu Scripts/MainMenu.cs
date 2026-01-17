19546784b88feec7e61be9a7b64232f5b1375275
Tj <109777815+PntherArt@users.noreply.github.com>
2026-01-17 12:42:38 +1100

    cleaning up scripts + load ui into menu
    main menu now has load ui
    moved scripts into folders in script folder

--- GDS_6_Ecliptic/Assets/Scripts/Menu Scripts/MainMenu.cs
+++ GDS_6_Ecliptic/Assets/Scripts/Menu Scripts/MainMenu.cs
@@ -11,9 +11,7 @@ public class MainMenu : MonoBehaviour
 
     public string level;
 
-    public GameObject[] canvas;
-
-    public GameObject[] controllerCanvas;
+    public GameObject canvas;
 
     public Animator anim;
     public Animator anim2;
@@ -22,7 +20,6 @@ public class MainMenu : MonoBehaviour
     public EventSystem eventSystem;
 
     public GameObject PauseUI;
-    public GameObject SettingsFirstSelect;
     public GameObject PauseFirstSelect;
 
     private bool connected = false;
@@ -38,8 +35,7 @@ public class MainMenu : MonoBehaviour
 
     private void Awake()
     {
-        //checks if the controller is connected
-        controllerCheck();
+
         Cursor.lockState = CursorLockMode.Locked;
  
     }
@@ -51,7 +47,6 @@ public class MainMenu : MonoBehaviour
 
     private void Update()
     {
-        controllerCheck();
         Debug.Log(eventSystem.currentSelectedGameObject);
         if (eventSystem.currentSelectedGameObject == null)
         {
@@ -71,38 +66,10 @@ public class MainMenu : MonoBehaviour
         
     }
 
-    public void ActivateDemoCanvas()
-    {
-        canvas[2].SetActive(true);
-
-        canvas[1].SetActive(false);
-        canvas[0].SetActive(false);
-    }
-    public void CtrLoad()
-    {
-        canvas[1].SetActive(true);
-        
-        canvas[0].SetActive(false);
-        //canvas[2].SetActive(false);
-    }
     /*public void SaveLoad()
     {
         canvas[0].SetActive(false);
         canvas[2].SetActive(true);
-    }*/
-
-    public void BackBtn()
-    {
-        canvas[0].SetActive(true);
-
-        canvas[1].SetActive(false);
-        //canvas[2].SetActive(false);
-    }
-
-    /*public void BackMenuBtn()
-    {
-        canvas[2].SetActive(false);
-        canvas[0].SetActive(true);
     }*/
 
     public void QuitGame()
@@ -124,40 +91,6 @@ public class MainMenu : MonoBehaviour
     public void ChangeFirstSelected(GameObject firstSelected)
     {
         eventSystem.SetSelectedGameObject(firstSelected);
-        //Using the new input system for UI, swaps the selected btn depending on which canvas is active
-        /*if (eventSystem.currentSelectedGameObject == SettingsFirstSelect)
-        {
-            //Debug.Log("ChangeBack");
-            eventSystem.SetSelectedGameObject(PauseFirstSelect);
-
-        }
-        else
-        {
-            //Debug.Log("Change");
-            eventSystem.SetSelectedGameObject(SettingsFirstSelect);
-        }*/
     }
-
-    public void controllerCheck()
-    {
-        // checks if the controller is connected
-            var controllers = Input.GetJoystickNames();
-            if (connected && controllers.Length > 0)
-            {
-                connected = false;
-                
-                controllerCanvas[0].SetActive(true);
-                controllerCanvas[1].SetActive(false);
-            Debug.Log("Connected");
-
-            }
-            else if (!connected && controllers.Length == 0)
-            {
-                connected = true;
-                controllerCanvas[0].SetActive(false);
-                controllerCanvas[1].SetActive(true);
-                Debug.Log("Disconnected");
-            }
-        }
 
 }
